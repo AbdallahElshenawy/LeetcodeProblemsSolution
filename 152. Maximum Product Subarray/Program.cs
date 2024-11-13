2d7bdcc74ac9c1c8d0dfba89838724565f9201dd
Abdallah <elshenawya894@gmail.com>
2024-11-13 20:51:33 +0200

    Create Program.cs

---
+++ 152. Maximum Product Subarray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int MaxProduct(int[] nums)
+    {
+        int n = nums.Length;
+        int leftProduct=1, rightProduct=1;
+        int maxProduct = nums[0];
+
+        for(int i = 0; i < n; i++)
+        {
+            if (leftProduct==0) leftProduct = 1;
+            if (rightProduct==0) rightProduct = 1;
+            leftProduct *= nums[i]; 
+            rightProduct *= nums[n-1-i];
+            maxProduct = Math.Max(maxProduct,Math.Max(leftProduct,rightProduct));
+        }
+        return maxProduct;
+
+    }
+}
