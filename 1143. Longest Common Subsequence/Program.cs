15e558bda0a261e1e573b2f786fc204f76d5f8f2
Abdallah <elshenawya894@gmail.com>
2024-11-14 08:02:04 +0200

    Problem n1143

---
+++ 1143. Longest Common Subsequence/Program.cs
@@ -0,0 +1,19 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+       int m = text1.Length;
+       int n = text2.Length;
+       int[,] result = new int[m+1, n+1];
+       
+        for (int i = 1; i <=m; i++)
+            for (int j = 1; j <=n; j++)
+            {
+                if (text1[i-1] == text2[j-1])
+                    result[i,j] = result[i-1,j-1] + 1;
+                else
+                    result[i,j]= Math.Max(result[i - 1, j], result[i,j-1]);
+            }
+        return result[m,n];
+    }
+}
