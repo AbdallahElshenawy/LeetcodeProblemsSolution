6bc49673c3deac2ab4ea1bda605fb0950996ceab
Abdallah <elshenawya894@gmail.com>
2024-11-21 06:50:21 +0200

    Create Program.cs

---
+++ 217. Contains Duplicate/Program.cs
@@ -0,0 +1,18 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+
+        HashSet<int> Duplicate = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (Duplicate.Contains(num))
+            {
+                return true;
+            }
+            Duplicate.Add(num);
+        }
+
+        return false;
+    }
+}
