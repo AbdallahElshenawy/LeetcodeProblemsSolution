7425fd50e42ef4fd005bde26f57b25a2fc86f6e9
Abdallah <elshenawya894@gmail.com>
2024-11-21 06:50:16 +0200

    Create Program.cs

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,23 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] < buyPrice)
+            {
+                buyPrice = prices[i];
+            }
+            else
+            {
+                int currentProfit = prices[i] - buyPrice;
+                profit = Math.Max(currentProfit, profit);
+            }
+        }
+
+        return profit;
+    }
+}
