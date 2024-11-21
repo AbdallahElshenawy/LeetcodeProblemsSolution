6ecf363aa04e76b9fc7b6ac3991ba3c1b15e9089
Abdallah <elshenawya894@gmail.com>
2024-11-21 06:50:27 +0200

    Create Program.cs

---
+++ 643. Maximum Average Subarray I/Program.cs
@@ -0,0 +1,23 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+        int maxSum = sum;
+        int firstElement = 0;
+        int endElement = k;
+        while (endElement < nums.Length) 
+        {
+            sum -= nums[firstElement];
+            firstElement++;
+            sum += nums[endElement];
+            endElement++;
+            maxSum = Math.Max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+        
+
+    }
+}
