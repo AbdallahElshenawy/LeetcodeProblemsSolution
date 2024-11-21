a2a53dc03c4df7b9ac9cf63c92c1bdc1a4ecea8d
Abdallah <elshenawya894@gmail.com>
2024-11-21 06:50:24 +0200

    Create Program.cs

---
+++ 283. Move Zeroes/Program.cs
@@ -0,0 +1,20 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int nonZeroElement = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[nonZeroElement] = nums[i];
+                nonZeroElement++;
+            }
+        }
+        for (int i = nonZeroElement; i < nums.Length; i++)
+        { nums[i] = 0; }
+
+
+
+    }
+}
