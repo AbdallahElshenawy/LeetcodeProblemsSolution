6addffa36473f78446b0d722648b45b9afe893af
Abdallah <elshenawya894@gmail.com>
2024-11-21 06:50:30 +0200

    Create Program.cs

---
+++ 2461. Maximum Sum of Distinct Subarrays With Length K/Program.cs
@@ -0,0 +1,32 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public long MaximumSubarraySum(int[] nums, int k)
+    {
+
+        long maxSum = 0;
+        long currentSum = 0;
+        HashSet<int> windowElements = new HashSet<int>();
+        int start = 0;
+
+        for (int end = 0; end < nums.Length; end++)
+        {
+            while (windowElements.Contains(nums[end]) || windowElements.Count >= k)
+            {
+                windowElements.Remove(nums[start]);
+                currentSum -= nums[start];
+                start++;
+            }
+
+            windowElements.Add(nums[end]);
+            currentSum += nums[end];
+
+            if (windowElements.Count == k)
+            {
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+        }
+
+        return maxSum;
+    }
+
+}
