df90650227fe366d4fa515e5ce4394d7799bc735
Abdallah <elshenawya894@gmail.com>
2024-11-25 19:55:22 +0200

    Create Program.cs

---
+++ 153. Find Minimum in Rotated Sorted Array/Program.cs
@@ -0,0 +1,20 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int FindMin(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return nums[left];
+
+    }
+
+
+}
