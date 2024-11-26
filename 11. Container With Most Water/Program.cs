aeaa30bba0b2c4eb488d65caa476d54c0678943d
Abdallah <elshenawya894@gmail.com>
2024-11-26 18:05:39 +0200

    Create Program.cs

---
+++ 11. Container With Most Water/Program.cs
@@ -0,0 +1,22 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int MaxArea = 0;
+        while (left < right)
+        {
+            int area = Math.Min(height[left],height[right])*(right - left);
+
+            MaxArea = Math.Max(area, MaxArea);
+
+            if (height[left] < height[right]) left++;
+
+            else right--;
+        }
+        return MaxArea;
+    }
+
+
+}
