fcfa02d9be14be57f3ba0664787a146e93c5e891
Abdallah <elshenawya894@gmail.com>
2024-12-16 17:01:36 +0200

    Create Program.cs

---
+++ 322. Coin Change/Program.cs
@@ -0,0 +1,25 @@
+﻿public class LeetcodeProblemsSolution
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        if(amount < 1) return 0;
+        int[] minCoins = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++)
+        {
+            minCoins[i] = int.MaxValue; 
+            foreach (int coin in coins)
+            {
+                if(coin <= i && minCoins[i-coin] != int.MaxValue)
+                {
+                    minCoins[i] = Math.Min(minCoins[i], 1 + minCoins[i - coin]);
+                }      
+            }
+        }
+        if (minCoins[amount]==int.MaxValue)
+            return -1;
+        return minCoins[amount];
+    }
+
+
+}
