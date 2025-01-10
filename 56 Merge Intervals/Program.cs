ec2373ecd752ef1e1c8fe1033e56820859caf95f
Abdallah <elshenawya894@gmail.com>
2025-01-10 14:02:15 +0200

    Create Program.cs

---
+++ 56 Merge Intervals/Program.cs
@@ -0,0 +1,35 @@
+﻿public class LeetcodeProblemsSolution
+
+{ 
+    public int[][] Merge(int[][] intervals)
+    {
+        if (intervals.Length <= 1)
+            return intervals;
+
+        // Sort the intervals by their starting point.
+        intervals = intervals.OrderBy(interval => interval[0]).ToArray();
+
+        List<int[]> result = new List<int[]>();
+
+        int[] newInterval = intervals[0];
+        result.Add(newInterval);
+
+        foreach (int[] interval in intervals)
+        {
+            // If the current interval overlaps with the last interval in the result,
+            // merge them by updating the end time of the last interval.
+            if (interval[0] <= newInterval[1])
+            {
+                newInterval[1] = Math.Max(newInterval[1], interval[1]);
+            }
+            else
+            {
+                // If the current interval does not overlap, add it to the result list.
+                newInterval = interval;
+                result.Add(newInterval);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
